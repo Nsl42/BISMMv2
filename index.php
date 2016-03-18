1f863532f80ecb39d5935c3264b66057841d68bc
nsl42 <nslmdrhr@gmail.com>
2016-03-18 18:08:40 +0100

    First Commit, Navigation and Title bar

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+    <head>
+        <title>.:: BiSMM - <?= $menu[5] ?></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="Content-Style-Type" content="text/css" />
+        <meta http-equiv="Content-Language" content="fr" />
+        <meta http-equiv="Pragma" content="no-cache"/>
+        <link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="css/bootstrap-theme.css" type="text/css" media="screen" />
+        <link rel="icon" type="image/png" href="/img/icon.png" /> 
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+            <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+    </head>
+<body>
+    <nav class="navbar navbar-default">
+            <a class="navbar-brand" href="#">
+          <h3 col-xs-2>BISMMv2</h3>
+    </a>
+<span class="col-xs-10">
+            <h3>Bioinformatique Structurale et Modélisation Moléculaire
+<br \> CRBM-CNRS Montpellier</h3>
+</span>
+    </nav>
+    <nav class="navbar navbar-default navigation">
+            <ul class="nav nav-justified">
+                <li><a href="#">Research</a></li>
+                <li><a href="#">People</a></li>
+                <li><a href="#">Tools</a></li>
+                <li><a href="#">Databases</a></li>
+            </ul>
+    </nav>
+
+    <div class="container">
+
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
